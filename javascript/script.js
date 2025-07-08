53238409aaaa1696bf4479852f99869b08357198
Nico <n.rossi.studio@gmail.com>
2025-07-08 09:38:06 +0100

    Cleaning  and tyding up the code

--- javascript/script.js
+++ javascript/script.js
@@ -13,19 +13,25 @@ function copyToClipboard(text) {
     .catch(err => console.error('Error copying:', err));
 }
 
-// 3. Seleziono il <main>
+// 3. Funzione per formato data
+
+function formatRecord(counterNumber) {
+  return `Value: ${counterNumber} - ${new Date().toLocaleString()}`;
+}
+
+// 4. Seleziono il <main>
 const mainTag = document.querySelector('main');
 
-// 3. Dichiaro il valore iniziale del counter
+// 4. Dichiaro il valore iniziale del counter
 let counterNumber = 0;
 
-// 4. Creo il contenitore (vuoto) instructions a scomparsa (settato nel .scss)
+// 6. Creo il contenitore (vuoto) instructions a scomparsa (settato nel .scss)
 const instructions = newElement(mainTag, 'div', 'instructions', '');
 
-// 4.a. Aggiungo il titolo <h3>
+// 6.a. Aggiungo il titolo <h3>
 const instructionsTitle = newElement(instructions, 'h3', '', 'Instructions');
 
-// 4.b. Aggiungo il testo descrittivo dentro un <p>
+// 6.b. Aggiungo il testo descrittivo dentro un <p>
 const instructionsText = newElement(
   instructions,
   'p',
@@ -35,10 +41,10 @@ Click "Record" to save the current counter value.
 Click "Reset" to reset the counter to zero.`
 );
 
-// 5. Creo il contenitore dei bottoni secondari
+// 7. Creo il contenitore dei bottoni secondari
 const counterSecondary = newElement(instructions, 'div', 'counterSecondary', '');
 
-// 6. Aggiungo i bottoni dentro il contenitore secondario
+// 8. Aggiungo i bottoni dentro il contenitore secondario
 const resetButton = newElement(counterSecondary, 'button', 'secondaryButton', 'Reset');
 resetButton.setAttribute('title', 'Reset the counter to 0'); // Aggiungo: "title" per tooltip visivo per migliorare acessibilità.
 resetButton.setAttribute('aria-label', 'Reset the counter to 0'); // Aggiungo: "aria-label" letto da screen-reader per migliorare acessibilità.
@@ -46,11 +52,14 @@ const recordButton = newElement(counterSecondary, 'button', 'secondaryButton', '
 recordButton.setAttribute('title', 'Save the current counter value');// Aggiungo: "title" con descrizione bottone per migliorare acessibilità.
 recordButton.setAttribute('aria-label', 'Save the current counter value')// Aggiungo: "aria-label" letto da screen-reader per migliorare acessibilità.
 
-// 7. Creo il contenitore dei bottoni e del display
+// 9. Creo il contenitore dei bottoni e del display.
 const counterPrimary = newElement(mainTag, 'div', 'counterPrimary', '');
 
-// 8. Creo i bottoni +, display, e - nello stesso contenitore
+// 10. Creo i bottoni +, display, e - nello stesso contenitore
 const plusButton = newElement(counterPrimary, 'button', 'primaryButton', '+');
+
+//10.1 Dichiaro gli attributi degli elementi creati.
+
 plusButton.setAttribute('title', 'Increase the counter by 1'); // Aggiungo: "title" per tooltip visivo per migliorare acessibilità.
 plusButton.setAttribute('aria-label', 'Increase the counter by 1'); // Aggiungo: "aria-label" letto da screen-reader per migliorare acessibilità.
 const counterDisplay = newElement(counterPrimary, 'div', 'counterDisplay', counterNumber);
@@ -59,7 +68,7 @@ minusButton.setAttribute('title', 'Decrease the counter by 1');// Aggiungo: "tit
 minusButton.setAttribute ('aria-label', 'Decrease the counter by 1'); // Aggiungo: "aria-label" letto da screen-reader per migliorare acessibilità.
 
 
-// 9. Aggiungo funzionalità ai bottoni
+// 11. Aggiungo funzionalità ai bottoni
 plusButton.addEventListener('click', () => {
   counterNumber++;
   counterDisplay.textContent = counterNumber;
@@ -92,13 +101,13 @@ resetButton.addEventListener('click', () => {
   }
 });
 
-// 10. Creo il contenitore dei record registrati
+// 12. Creo il contenitore dei record registrati
 const counterRecordsContainer = newElement(mainTag, 'div', 'counterRecordsContainer', '');
 
-// 10.a. Aggiungo il titolo <h3>
+// 12.a. Aggiungo il titolo <h3>
 const recordsTitle = newElement(counterRecordsContainer, 'h3', '', 'Recorded clicks');
 
-// 10.b. Aggiungo il testo descrittivo dentro un <p>
+// 12.b. Aggiungo il testo descrittivo dentro un <p>
 const counterRecordsText = newElement(
   counterRecordsContainer,
   'p',
@@ -106,13 +115,7 @@ const counterRecordsText = newElement(
   `See below the counted clicks recorded to keep track of your counting.`
 );
 
-// 12. Funzione per formato data
-
-function formatRecord(counterNumber) {
-  return `Value: ${counterNumber} - ${new Date().toLocaleString()}`;
-}
-
-// 12.a. Creo le card per la visualizzazione dei record e della data
+// 13.a. Creo le card per la visualizzazione dei record e della data
 recordButton.addEventListener('click', () => {
   if (counterNumber !== 0) {
     counterRecordsContainer.classList.add('visible');
@@ -127,31 +130,20 @@ recordButton.addEventListener('click', () => {
 
     const copyButton = newElement(recordCard, 'button', 'copyButtons', 'COPY');
     copyButton.setAttribute('title', 'Copy this record to clipboard');
-    copyButton.setAttribute('aria-label', 'Copy this record to clipboard'); // Aggiungo: "aria-label" letto da screen-reader per migliorare acessibilità.
-    
-  } else if (counterNumber === 0) {
-    const messageDiv = newElement(document.body, 'div', 'error-message', 'Can not record a value of 0') // Creo il messaggio di errore, migliora la UX.
-    messageDiv.setAttribute('role', 'alert'); // Aggiungo "role" per migliorare l'accessibilità.
-
-    setTimeout(()=> {
-      messageDiv.remove();
-    }, 700);
-    }
+    copyButton.setAttribute('aria-label', 'Copy this record to clipboard'); // Aggiungo: "aria-label" letto da screen-reader per migliorare acessibilità. 
+  }
 });
-
-
-   
 
 counterRecordsContainer.addEventListener('click', (event) => {
   const target = event.target;
 
-  // 13 Cancella la recordCard (parentElement indica quale specifica card eliminare: la parent)
+  // 14. Cancella la recordCard (parentElement indica quale specifica card eliminare: la parent)
   const deleteButton = target.closest('.deleteButtons');
     if (deleteButton) {
       deleteButton.parentElement.remove();
     }
 
- // Copia il testo dal data-record
+ // 15. Copia il testo dal data-record
   const copyButton = target.closest('.copyButtons');
   if (copyButton) { 
     const recordText = copyButton.parentElement.getAttribute('data-record');
